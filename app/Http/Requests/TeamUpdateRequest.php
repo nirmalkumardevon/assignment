9dd6a7cb53ff69843036562c13153be8af908916
nirmalkumardevon <98765465+nirmalkumardevon@users.noreply.github.com>
2022-02-02 18:39:20 +0530

    Updated request file

--- app/Http/Requests/TeamUpdateRequest.php
+++ app/Http/Requests/TeamUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TeamUpdateRequest extends FormRequest
+class TeamUpdateRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
