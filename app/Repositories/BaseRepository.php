23cc770cd48be1b72209fa3cc5c5e04ca890b6c5
nirmalkumardevon <98765465+nirmalkumardevon@users.noreply.github.com>
2022-02-09 12:14:18 +0530

    Added comments in Base Repository

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Interfaces\TeamRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseRepository implements TeamRepositoryInterface {
@@ -22,26 +23,46 @@ class BaseRepository implements TeamRepositoryInterface {
         $this->model = $model;
     }
 
+    /**
+     * @return Collection
+     */
     public function all()
     {
         return $this->model->all();
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function get($id)
     {
         return $this->model->findOrFail($id);
     }
 
+    /**
+     * @param $id
+     * @return int
+     */
     public function delete($id)
     {
         return $this->model->destroy($id);
     }
 
+    /**
+     * @param array $details
+     * @return mixed
+     */
     public function create(array $details)
     {
         return $this->model->create($details);
     }
 
+    /**
+     * @param $id
+     * @param array $details
+     * @return mixed
+     */
     public function update($id, array $details)
     {
         return $this->model->whereId($id)->update($details);
