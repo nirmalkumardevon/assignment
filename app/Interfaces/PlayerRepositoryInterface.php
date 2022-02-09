324f03520ca57fced3ea8d5ee8d2f47dfbca0356
nirmalkumardevon <98765465+nirmalkumardevon@users.noreply.github.com>
2022-02-09 12:16:30 +0530

    Updated comments interface

--- app/Interfaces/PlayerRepositoryInterface.php
+++ app/Interfaces/PlayerRepositoryInterface.php
@@ -9,10 +9,39 @@ use Illuminate\Http\JsonResponse;
 
 interface PlayerRepositoryInterface
 {
+    /**
+     * @return mixed
+     */
     public function all();
+
+    /**
+     * @param $teamId
+     * @return mixed
+     */
     public function get($teamId);
+
+    /**
+     * @param $teamId
+     * @return mixed
+     */
     public function getTeamPlayers($teamId);
+
+    /**
+     * @param $teamId
+     * @return mixed
+     */
     public function delete($teamId);
+
+    /**
+     * @param array $teamDetails
+     * @return mixed
+     */
     public function create(array $teamDetails);
+
+    /**
+     * @param $orderId
+     * @param array $teamDetails
+     * @return mixed
+     */
     public function update($orderId, array $teamDetails);
 }
